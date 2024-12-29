dfe350383a709d6640df206f437a23fb4730f394
Ewin <76159978+EwinLit@users.noreply.github.com>
2024-12-29 19:31:09 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 rm -rf ~/.filesearch
 mkdir ~/.filesearch
 cp -r ./filesearch-qt5 ~/.filesearch/
+cp -r ./database_work ~/.filesearch
+cp -r ./extract_keywords ~/.filesearch
 cd ~/.filesearch/filesearch-qt5
 qmake filesearch.pro
 make clean
