ea25f4ca78c8567971ac490a661c8d90a053bfc1
tyy_1117 <90208630+Zivina@users.noreply.github.com>
2024-12-30 13:22:45 +0800

    Update refresh.py

--- .filesearch/extract_keywords/refresh.py
+++ .filesearch/extract_keywords/refresh.py
@@ -1,5 +1,39 @@
 import os
+import extract_keywords, create_database
+from pathlib import Path
+import sqlite3
 
-# 创建一个空文件
-open('example.txt', 'a').close()
+if __name__ == "__main__":
+    # 文档关键词提取
+    text_path = Path("./text_files")
+    valid_extensions = {".txt", ".docx", ".pdf"}
+    for file_path in text_path.rglob("*"):
+        if file_path.is_file() and text_path.suffix.lower() in valid_extensions:
+            output_name = file_path.stem
+            extract_keywords.main(file_path, Path("./keywords") / f"{output_name}.txt")
 
+    # 数据库创建
+    # 构建文档-关键词字典
+    input_directory = "keywords"
+    documents, file_paths = create_database.build_documents_dictionary(input_directory)
+
+    # 创建 SQLite 数据库
+    db_path = "keywords.db"
+    create_database.create_database(db_path)
+
+    # 添加文档到数据库
+    create_database.add_data_to_database(db_path, documents, file_paths)
+
+    conn = conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    # 查询所有数据
+    cursor.execute("SELECT * FROM keyword")
+    rows = cursor.fetchall()
+
+    # 打印数据
+    for row in rows:
+        print(row)
+
+    # 关闭连接
+    conn.close()
